7313b06b454f707f065cf836bfce8063a4807d86
mohamed mamdouh <moMamdouhSaad@gmail.com>
2020-08-15 16:46:26 +0200

    added bootstrap and button module

--- libs/ui/src/lib/elements/button/button/button.component.ts
+++ libs/ui/src/lib/elements/button/button/button.component.ts
@@ -1,13 +1,52 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  Renderer2,
+  ElementRef,
+  HostBinding,
+} from '@angular/core';
+import {
+  BootstrapButtonMainColors,
+  BootstrapButtonSizes,
+  BootstrapButtonStates,
+} from '../../../utils/utils';
 
 @Component({
-  selector: 'pa-pos-button',
-  templateUrl: './button.component.html',
+  // tslint:disable-next-line: component-selector
+  selector: 'button[btnColor] ',
+  template: '<ng-content></ng-content>',
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent implements OnInit {
-  public constructor() {}
+export class ButtonComponent {
+  public constructor(
+    private readonly renderer: Renderer2,
+    private readonly el: ElementRef
+  ) {}
 
-  public ngOnInit(): void {}
+  @Input() public set btnColor(color: BootstrapButtonMainColors) {
+    this.renderer.addClass(this.el.nativeElement, 'btn');
+    if (color && BootstrapButtonMainColors[color]) {
+      this.renderer.addClass(
+        this.el.nativeElement,
+        BootstrapButtonMainColors[color]
+      );
+    }
+  }
+
+  @Input() public set btnSize(size: BootstrapButtonSizes) {
+    if (size && BootstrapButtonSizes[size]) {
+      this.renderer.addClass(this.el.nativeElement, BootstrapButtonSizes[size]);
+    }
+  }
+
+  @Input() public set btnState(state: BootstrapButtonStates) {
+    if (state && BootstrapButtonStates[state]) {
+      this.renderer.addClass(
+        this.el.nativeElement,
+        BootstrapButtonStates[state]
+      );
+    }
+  }
 }
---
+++ libs/ui/src/lib/utils/utils.ts
@@ -0,0 +1,31 @@
+export enum BootstrapButtonMainColors {
+  primary = 'btn-primary',
+  secondary = 'btn-secondary',
+  success = 'btn-success',
+  danger = 'btn-danger',
+  warning = 'btn-warning',
+  info = 'btn-info',
+  light = 'btn-light',
+  dark = 'btn-dark',
+  link = 'btn-link',
+  outlinePrimary = 'btn-outline-primary',
+  outlineSecondary = 'btn-outline-secondary',
+  outlineSuccess = 'btn-outline-success',
+  outlineDanger = 'btn-outline-danger',
+  outlineWarning = 'btn-outline-warning',
+  outlineInfo = 'btn-outline-info',
+  outlineLight = 'btn-outline-light',
+  outlineDark = 'btn-outline-dark',
+  outlineLink = 'btn-outline-link',
+}
+
+export enum BootstrapButtonSizes {
+  small = 'btn-sm',
+  large = 'btn-lg',
+  block = 'btn-block',
+}
+
+export enum BootstrapButtonStates {
+  active = 'active',
+  disabled = 'disabled',
+}
